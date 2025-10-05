9c5b0a1687c247344a9139b6b811773038f75157
rinevard <rinevard@outlook.com>
2025-10-06 00:07:46 +0800

    相机平滑

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,7 +1,8 @@
 extends Camera2D
 
 @export var follow_target: Node2D
+@export var smoothing_speed: float = 5.0
 
 func _physics_process(delta: float) -> void:
 	if follow_target:
-		global_position = follow_target.global_position
+		global_position = global_position.lerp(follow_target.global_position, smoothing_speed * delta)
