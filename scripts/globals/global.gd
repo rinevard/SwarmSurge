8f9843b63d78244bcd9193a32dd28a890e0fbbbb
rinevard <rinevard@outlook.com>
2025-10-05 18:56:17 +0800

    暂停菜单外观

--- scripts/globals/global.gd
+++ scripts/globals/global.gd
@@ -10,6 +10,8 @@ var bullet_handler: BulletHandler = null
 var player_master: SwarmMaster = null
 var enemy_masters: Array[EnemyMaster] = []
 
+var game_paused: bool = false
+
 func reset_game_data(game: Game) -> void:
 	bullet_handler = game.bullet_handler
 	player_master = null
